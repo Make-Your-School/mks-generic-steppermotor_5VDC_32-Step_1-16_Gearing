21889b69dd136e30961d7689b1535cda9b8d107c
s-light <git@s-light.eu>
2026-02-20 21:57:41 +0100

    bulk-update examples

--- examples/steppermotor_5VDC_32-Step_1-16_Gearing_minimal/steppermotor_5VDC_32-Step_1-16_Gearing_minimal.ino
+++ examples/steppermotor_5VDC_32-Step_1-16_Gearing_minimal/steppermotor_5VDC_32-Step_1-16_Gearing_minimal.ino
@@ -1,26 +1,29 @@
-// Laden der Motortreiber-Bibliothek
+// verbinde den Motortreiber mit einem der I2C Steckplätze auf dem Grove shield.
+// schließe einen 5V Stepper-Motor am Pin-Header an.
+// wenn der StepperMotor 5 Kabel hat, bleibt wahrscheinlich das Rote-Kabel
+// übrig. dieses hat dann keine Verbindung zum Motortreiber. installiere die
+// Grove_I2C_Motor_Driver library mithilfe des Library-Managers. lade den Sketch
+// auf den Arduino drücke den Reset Knopf auf dem Motortreiber und dem Arduino
+// nun sollten sich der Stepper-Motor wie unten beschrieben Drehen
+
+// einbinden der Motortreiber-Bibliothek
 #include <Grove_I2C_Motor_Driver.h>
 
 // Das ist die I2C Standardadresse
 #define I2C_ADDRESS 0x0f
 
 void setup() {
-  // Baue eine serielle Verbindung mit dem Laptop auf
-  Serial.begin(9600);
-
-  // Verbinde mit Motortreiber
-  Motor.begin(I2C_ADDRESS);
+    // Verbinde mit Motortreiber
+    Motor.begin(I2C_ADDRESS);
 }
 
 void loop() {
-  // Drehe etwas im Uhrzeigersinn
-  Motor.StepperRun(128);
-  // Warte
-  delay(500);
-  // Drehe gegen den Uhrzeigersinn
-  Motor.StepperRun(-128);
-  // Warte
-  delay(500);
+    // Drehe 512 Schritte im Uhrzeigersinn
+    Motor.StepperRun(512);
+    // Warte eine halbe Sekunde
+    delay(500);
+    // Drehe 1024 Schritte gegen den Uhrzeigersinn
+    Motor.StepperRun(-1024);
+    // Warte eine halbe Sekunde
+    delay(500);
 }
-
-
